75285bfbc89df11d32439b0f921c91f6ed460642
Brienna Klassen <Brienna.Klassen@gmail.com>
2021-04-28 13:33:13 -0700

    Add Logout button

--- app/src/Account.js
+++ app/src/Account.js
@@ -1,29 +1,37 @@
 import React, { useState, useEffect } from "react";
 
-import GoogleLogin from "react-google-login";
+import { GoogleLogin, GoogleLogout } from "react-google-login";
 
 function Account() {
+  const [isSignedIn, setIsSignedIn] = useState(false);
   const [user, setUser] = useState({});
 
   function responseGoogle(response) {
     console.log(response);
     setUser(response.profileObj);
+    setIsSignedIn(true);
+  }
+
+  function logOut() {
+    setUser({});
+    setIsSignedIn(false);
+    console.log("logout successful");
   }
 
   useEffect(() => {
     console.log(user);
-  }, [user]);
+  }, [user, isSignedIn]);
 
   return (
     <div>
-      {user !== {} ? (
+      {isSignedIn ? (
         <>
           <h2>Logged in as...</h2>
           <img id="profilePic" alt="profile photo" src={user.imageUrl} />
           <h3> {user.name} </h3>
         </>
       ) : (
-        `<p>User not found...</p>`
+        <p>Login to use this app!</p>
       )}
       <GoogleLogin
         clientId="672610100992-9ob4krjvvh9kfs8sr0jb4d7hlsvcmcv8.apps.googleusercontent.com"
@@ -32,6 +40,8 @@ function Account() {
         onFailure={responseGoogle}
         cookiePolicy={"single_host_origin"}
       />
+
+      <button onClick={logOut}>Logout</button>
     </div>
   );
 }
