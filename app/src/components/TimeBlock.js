3f8def0583da859210ca81400309407bec9dff43
Brienna Klassen <Brienna.Klassen@gmail.com>
2021-05-05 13:26:43 -0700

    adjust TimeBlock getGridPlacement() function

--- app/src/components/TimeBlock.js
+++ app/src/components/TimeBlock.js
@@ -1,16 +1,24 @@
 import React, { useState, useEffect } from "react";
 
 const TimeBlock = ({ start, end, summary, dayStart }) => {
-  var blockStart =
-    Math.round(new Date(start).getHours() * 2) / 2 - dayStart.getHours();
-  blockStart = blockStart % 1 !== 0 ? blockStart++ : blockStart;
-  var blockEnd =
-    Math.round(new Date(end).getHours() * 2) / 2 - dayStart.getHours();
-  blockEnd = blockEnd % 1 !== 0 ? blockEnd++ : blockEnd;
+  function getGridPlacement() {
+    console.debug({ start }); //test
+    var blockStart = (new Date(start).getHours() - dayStart.getHours()) * 2;
+    console.debug({ blockStart });
+    blockStart = blockStart % 1 !== 0 ? blockStart + 2 : blockStart + 1;
+    console.debug({ blockStart });
+
+    console.debug({ end }); //test
+    var blockEnd = (new Date(end).getHours() - dayStart.getHours()) * 2;
+    console.debug({ blockEnd });
+    blockEnd = blockEnd % 1 !== 0 ? blockEnd + 1 : blockEnd;
+    console.debug({ blockEnd });
+
+    return `${blockStart} / ${blockEnd}`;
+  }
 
   var eventMeasurements = {
-    gridRowStart: `${blockStart}`,
-    gridRowEnd: `${blockEnd}`,
+    gridRow: getGridPlacement(),
     gridColumn: "span 1",
   };
 
