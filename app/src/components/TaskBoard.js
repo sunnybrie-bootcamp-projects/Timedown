30b755cc60024ffac47eb6996c54cb3fe5f53cd4
Brienna Klassen <brienna.klassen@gmail.com>
2021-05-21 15:57:42 -0700

    styling, addtoGoogle function

--- app/src/components/TaskBoard.js
+++ app/src/components/TaskBoard.js
@@ -28,7 +28,7 @@ function TaskBoard({
     <table
       className="taskBoard"
       style={{
-        display: tab === "taskboard" ? "inline-table" : "none",
+        display: tab === "taskboard" ? "table" : "none",
       }}
     >
       <caption>
